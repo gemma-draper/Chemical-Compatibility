e7b4287a9ba4164cb6d52c63b184cb154d46469a
Gemma Draper <g.l.draper@outlook.com>
2021-12-06 17:20:25 +0000

    New file for make_tables.

---
+++ make_tables.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# make tables!
+
+chemical_name_table = pd.DataFrame()
+chemical_formula_table = pd.DataFrame()
+chemical_hazard_table = pd.DataFrame()
+
+material_name_table = pd.DataFrame()
+source_table = pd.DataFrame()
+
+# for chemical
+#   nist search
+#   get formula from first item
+#   check if in formula table
+#       add to table, generating new ID
+#       or 
+
+
